3291d83c8c1b4862c41f3f8bde33e5522006c07e
Mike Rivnak <rivnakm1@gmail.com>
2023-10-19 19:28:36 -0400

    start lexer

---
+++ libcashew-parser/src/cashew_parser.cpp
@@ -0,0 +1,1 @@
+#include "cashew_parser.hpp"
---
+++ tests/libcashew-parser/lexer.cpp
@@ -0,0 +1,7 @@
+#include <cashew_parser.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("hello returns Cashew", "[hello]")
+{
+    REQUIRE("Cashew" == "Cashew");
+}
