57c84668559a95af9408ade4f3e0c7c06d06c72a
Mike Rivnak <rivnakm1@gmail.com>
2023-11-06 01:02:02 -0500

    test float literals

--- tests/src/libcashew-parser/lexer.cpp
+++ tests/src/libcashew-parser/lexer.cpp
@@ -249,3 +249,84 @@ TEST_CASE("tokenize (int_literal_4.nut)", "[token]")
         REQUIRE(tokens[i].value == expected[i].value);
     }
 }
+
+TEST_CASE("tokenize (float_literal_1.nut)", "[token]")
+{
+    auto input = readFileToStream("../tests/examples/parser/float_literal_1.nut");
+
+    std::vector<Token> expected = {// let a = 5.0
+                                   {TokenType::TOKEN_LET},        {TokenType::TOKEN_IDENTIFIER, "a"},
+                                   {TokenType::TOKEN_ASSIGNMENT}, {TokenType::TOKEN_FLOAT_LITERAL, "5.0"},
+                                   {TokenType::TOKEN_NEWLINE},    {TokenType::TOKEN_END_OF_FILE}};
+    auto tokens = tokenize(input);
+
+    REQUIRE(tokens.size() == expected.size());
+    for (size_t i = 0; i < tokens.size(); ++i)
+    {
+        REQUIRE(tokens[i].type == expected[i].type);
+        REQUIRE(tokens[i].value == expected[i].value);
+    }
+}
+
+TEST_CASE("tokenize (float_literal_2.nut)", "[token]")
+{
+    auto input = readFileToStream("../tests/examples/parser/float_literal_2.nut");
+
+    std::vector<Token> expected = {// let a = 5_000.000_1
+                                   {TokenType::TOKEN_LET},        {TokenType::TOKEN_IDENTIFIER, "a"},
+                                   {TokenType::TOKEN_ASSIGNMENT}, {TokenType::TOKEN_FLOAT_LITERAL, "5_000.000_1"},
+                                   {TokenType::TOKEN_NEWLINE},    {TokenType::TOKEN_END_OF_FILE}};
+    auto tokens = tokenize(input);
+
+    REQUIRE(tokens.size() == expected.size());
+    for (size_t i = 0; i < tokens.size(); ++i)
+    {
+        REQUIRE(tokens[i].type == expected[i].type);
+        REQUIRE(tokens[i].value == expected[i].value);
+    }
+}
+
+TEST_CASE("tokenize (float_literal_3.nut)", "[token]")
+{
+    auto input = readFileToStream("../tests/examples/parser/float_literal_3.nut");
+
+    std::vector<Token> expected = {// let a = 5.
+                                   {TokenType::TOKEN_LET},        {TokenType::TOKEN_IDENTIFIER, "a"},
+                                   {TokenType::TOKEN_ASSIGNMENT}, {TokenType::TOKEN_FLOAT_LITERAL, "5."},
+                                   {TokenType::TOKEN_NEWLINE},    {TokenType::TOKEN_END_OF_FILE}};
+    auto tokens = tokenize(input);
+
+    REQUIRE(tokens.size() == expected.size());
+    for (size_t i = 0; i < tokens.size(); ++i)
+    {
+        REQUIRE(tokens[i].type == expected[i].type);
+        REQUIRE(tokens[i].value == expected[i].value);
+    }
+}
+
+TEST_CASE("tokenize (float_literal_4.nut)", "[token]")
+{
+    auto input = readFileToStream("../tests/examples/parser/float_literal_4.nut");
+
+    std::vector<Token> expected = {// let a = 5.0_f32
+                                   {TokenType::TOKEN_LET},
+                                   {TokenType::TOKEN_IDENTIFIER, "a"},
+                                   {TokenType::TOKEN_ASSIGNMENT},
+                                   {TokenType::TOKEN_FLOAT_LITERAL, "5.0_f32"},
+                                   {TokenType::TOKEN_NEWLINE},
+                                   // let b = 5.0_f64
+                                   {TokenType::TOKEN_LET},
+                                   {TokenType::TOKEN_IDENTIFIER, "b"},
+                                   {TokenType::TOKEN_ASSIGNMENT},
+                                   {TokenType::TOKEN_FLOAT_LITERAL, "5.0_f64"},
+                                   {TokenType::TOKEN_NEWLINE},
+                                   {TokenType::TOKEN_END_OF_FILE}};
+    auto tokens = tokenize(input);
+
+    REQUIRE(tokens.size() == expected.size());
+    for (size_t i = 0; i < tokens.size(); ++i)
+    {
+        REQUIRE(tokens[i].type == expected[i].type);
+        REQUIRE(tokens[i].value == expected[i].value);
+    }
+}
