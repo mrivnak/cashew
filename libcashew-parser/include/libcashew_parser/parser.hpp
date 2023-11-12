3f13f7ad8c38d93049674a502314d19eaf9e3c77
Mike Rivnak <mrivnak@users.noreply.github.com>
2023-11-11 22:10:08 -0500

    Fix variant type

--- libcashew-parser/include/libcashew_parser/parser.hpp
+++ libcashew-parser/include/libcashew_parser/parser.hpp
@@ -12,7 +12,7 @@ namespace cashew::parser
  * @typedef literal
  * @brief A literal value; string, number, boolean, etc.
  */
-typedef std::variant<std::string, u_int8_t, u_int16_t, u_int32_t, u_int64_t, int8_t, int16_t, int32_t, ino64_t, float,
+typedef std::variant<std::string, u_int8_t, u_int16_t, u_int32_t, u_int64_t, int8_t, int16_t, int32_t, int64_t, float,
                      double, bool>
     literal;
 } // namespace cashew::parser
